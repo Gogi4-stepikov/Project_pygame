1b1bf4a5933655b60235e98915c4ec81d75ec20d
Gogi4-stepikov <gogieathotdoga@gmail.com>
2025-01-22 22:19:34 +0300

    Update main.py
    Основное меню

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def draw_character_settings_button(screen):
     screen.blit(chr_txt, (width // 2 - chr_txt.get_width() // 2 + 210, height // 2 - chr_txt.get_height() // 2 - 100))
     return chr_rect
 
-def draw_continue_button(screen):
+def draw_continue_button(screen): #добавить потом
     continue_button_rect = pygame.Rect(width // 2 - 100, height // 2 - 50, 200, 50)
     pygame.draw.rect(screen, pygame.Color("White"), continue_button_rect)
     continue_text = font.render("Продолжить", True, pygame.Color("Black"))
@@ -67,7 +67,7 @@ def draw_settings_button_for_cont(screen):
     return se_cont_bt
 
 def draw_settings_button_for_cont_ret(screen):
-    se_cont_text_ret = font.render("вернуться", True, pygame.Color("Black"))
+    se_cont_text_ret = font.render("Вернуться", True, pygame.Color("Black"))
     se_cont_bt_ret = pygame.Rect(10, 60, se_cont_text_ret.get_width() + 100, se_cont_text_ret.get_height() // 2 + 20)
     pygame.draw.rect(screen, pygame.Color("White"), se_cont_bt_ret)
     screen.blit(se_cont_text_ret, (se_cont_bt_ret.x + 50, se_cont_bt_ret.y + 5))
@@ -78,8 +78,13 @@ pygame.init()
 size = width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
 screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 font = pygame.font.Font(None, 36)
+pygame.mixer.music.load("musicbg.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+music_playing = True
 boy_image = pygame.image.load("boy.png").convert_alpha()
 girl_image = pygame.image.load("girl.png").convert_alpha()
+room_image = pygame.image.load("room.png").convert_alpha()
 image = boy_image
 chr_rect = draw_character_settings_button(screen)
 image_rect = image.get_rect(center=(chr_rect.centerx, chr_rect.bottom + image.get_height() // 2 + 20))
@@ -88,7 +93,7 @@ current_screen = "start"
 st_bt = None
 se_bt = None
 color_settings_rect = None
-music_settings_button_rect = None
+music_settings_rect = None
 se_re_bt = None
 continue_button_rect = None
 se_cont_bt = None
@@ -100,6 +105,8 @@ while running:
     if current_screen == "start":
         st_bt = draw_start_button(screen)
         se_bt = draw_settings_button(screen)
+        image_rect = image.get_rect(center=(chr_rect.centerx - 200, chr_rect.bottom + image.get_height() // 2 + 50))
+        screen.blit(image, image_rect)
     elif current_screen == "settings":
         draw_settings(screen)
         color_settings_rect = draw_color_settings_button(screen)
@@ -117,8 +124,12 @@ while running:
         screen.blit(image, image_rect)
         se_cont_bt_ret = draw_settings_button_for_cont_ret(screen)
     elif current_screen == "continue":
-        continue_button_rect = draw_continue_button(screen)
-        se_bt = draw_settings_button(screen)
+        room_image_scaled = pygame.transform.scale(room_image, (width, height))
+        screen.blit(room_image_scaled, (0, 0))
+        se_cont_bt = draw_settings_button_for_cont(screen)
+        image_scaled = pygame.transform.scale(image, (width // 4, height // 4))
+        image_rect = image_scaled.get_rect(center=(width // 2 + 300, height // 2 + 400))
+        screen.blit(image, image_rect)
     elif current_screen == "end":
         end_text = font.render("GAME OVER", True, pygame.Color("Black"))
         screen.blit(end_text, (width // 2 - end_text.get_width() // 2, height // 2 - end_text.get_height() // 2))
@@ -136,19 +147,33 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if current_screen == "start" and st_bt.collidepoint(event.pos):
                 current_screen = "continue"
-            elif current_screen == "continue" and se_cont_bt.collidepoint(event.pos):
-                current_screen = "settings_for_cont"
             elif current_screen == "start" and se_bt.collidepoint(event.pos):
                 current_screen = "settings"
             elif current_screen == "settings" and chr_rect.collidepoint(event.pos):
                 change_image = True
             elif current_screen == "settings" and color_settings_rect.collidepoint(event.pos):
                 bg_color = random_color_change()
+            elif current_screen == "settings" and music_settings_rect.collidepoint(event.pos):
+                if music_playing:
+                    pygame.mixer.music.pause()
+                    music_playing = False
+                else:
+                    pygame.mixer.music.unpause()
+                    music_playing = True
             elif current_screen == "settings" and se_re_bt.collidepoint(event.pos):
                 current_screen = "start"
-            elif current_screen == "continue" and continue_button_rect.collidepoint(event.pos):
-                current_screen = "end"
-                Traceback (most recent call last):
-  File "C:\Users\test344\PycharmProjects\pythonProject10\main.py", line 131, in <module>
-    elif current_screen == "continue" and se_cont_bt.collidepoint(event.pos):
-AttributeError: 'NoneType' object has no attribute 'collidepoint'
+            elif current_screen == "continue" and se_cont_bt.collidepoint(event.pos):
+                current_screen = "settings_for_cont"
+            elif current_screen == "settings_for_cont" and chr_rect.collidepoint(event.pos):
+                change_image = True
+            elif current_screen == "settings_for_cont" and color_settings_rect.collidepoint(event.pos):
+                bg_color = random_color_change()
+            elif current_screen == "settings_for_cont" and music_settings_rect.collidepoint(event.pos):
+                if music_playing:
+                    pygame.mixer.music.pause()
+                    music_playing = False
+                else:
+                    pygame.mixer.music.unpause()
+                    music_playing = True
+            elif current_screen == "settings_for_cont" and se_cont_bt_ret.collidepoint(event.pos):
+                current_screen = "continue"
